a147b1dc882e5754fefb552cbc140bff2f5fee1b
DineshKushwaha <dinukush18@gmail.com>
2020-05-22 09:58:07 +0800

    updated the config

--- sph_newsletter/src/Controller/PreviewNewsletterController.php
+++ sph_newsletter/src/Controller/PreviewNewsletterController.php
@@ -18,11 +18,20 @@ class PreviewNewsletterController extends ControllerBase {
    */
   public function previewPage($nid) {
 
+
+    $values = \Drupal::entityQuery('node')->condition('nid', $nid)->execute();
     // Generate the HTML and dislay Preview Web Page
-    $newsletter_html = $this->getHTML($nid);
-    $response = new Response();
-    $response->setContent($newsletter_html['newsletter_data']);
-    return $response;
+    if (isset($values) && !empty($values)) {
+      $newsletter_html = $this->getHTML($nid);
+      $response = new Response();
+      $response->setContent($newsletter_html['newsletter_data']);
+      return $response;
+
+    } else {
+      return [
+        '#markup' => 'Not a Valid nid',
+      ];
+    }
   }
 
   public function getHTML($nid)
@@ -34,33 +43,31 @@ class PreviewNewsletterController extends ControllerBase {
     $builder = \Drupal::entityTypeManager()->getViewBuilder($entity_type);
     $storage = \Drupal::entityTypeManager()->getStorage($entity_type);
     $node = $storage->load($nid);
-    if ($node->bundle() == 'newsletter') {
-      $build = $builder->view($node, $view_mode);
-      $cssFile = ($node->hasField('field_css_file_name')) ? $node->field_css_file_name->value : '';
-      $module_path = drupal_get_path('module', 'sph_newsletter');
-      $host = \Drupal::request()->getSchemeAndHttpHost();
+    $build = $builder->view($node, $view_mode);
+    $cssFile = ($node->hasField('field_css_file_name')) ? $node->field_css_file_name->value : '';
+    $module_path = drupal_get_path('module', 'sph_newsletter');
+    $host = \Drupal::request()->getSchemeAndHttpHost();
 
-      $renderable = [
-          '#theme' => 'newsletter__preview',
-          '#result' => $build,
-      ];
+    $renderable = [
+        '#theme' => 'newsletter__preview',
+        '#result' => $build,
+    ];
 
-      // generate the rendered HTML from the twig file
-      $newsletter_data = \Drupal::service('renderer')->renderPlain($renderable); // html output
-      $newsletter_data = (string)$newsletter_data;
+    // generate the rendered HTML from the twig file
+    $newsletter_data = \Drupal::service('renderer')->renderPlain($renderable); // html output
+    $newsletter_data = (string)$newsletter_data;
 
-      //Converting External css to Inline Css using Emogrifier
-      $css_path = $host . '/' . $module_path . '/css/' . $cssFile;
-      $css_content = file_get_contents($css_path);
-      $visualHtml = CssInliner::fromHtml($newsletter_data)->inlineCss($css_content)->render();
+    //Converting External css to Inline Css using Emogrifier
+    $css_path = $host . '/' . $module_path . '/css/' . $cssFile;
+    $css_content = file_get_contents($css_path);
+    $visualHtml = CssInliner::fromHtml($newsletter_data)->inlineCss($css_content)->render();
 
-      $newsletter_html = [
-          'newsletter_data' => $visualHtml,
-          'node' => $node,
-      ];
+    $newsletter_html = [
+        'newsletter_data' => $visualHtml,
+        'node' => $node,
+    ];
 
       return $newsletter_html;
     }
-  }
 
 }
