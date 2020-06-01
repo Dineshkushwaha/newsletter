47461b34b00a3f20c1c56f4e9e433b2cff23d690
Parag Bhilkar <parag.bhilkar@ext.saint-gobain.com>
2020-06-01 11:37:34 +0530

    added forms and routing

---
+++ sph_newsletter/src/Form/ArticleEditForm.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Drupal\sph_newsletter\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\node\Entity\Node;
+
+class ArticleEditForm extends ConfigFormBase {
+  const SETTINGS = 'example.settings';
+
+  public function getFormId()
+  {
+    return 'article_edit_form';
+    // TODO: Implement getFormId() method.
+  }
+
+  public function getEditableConfigNames()
+  {
+    // TODO: Implement getEditableConfigNames() method.
+    return [
+        static::SETTINGS,
+    ];
+
+  }
+  public function buildForm(array $form, FormStateInterface $form_state)
+  {
+    // TODO: Implement buildForm() method.
+    $nid = \Drupal::routeMatch()->getParameter('id');
+    $node = Node::load($nid);
+
+    $form['article_data'] = [
+        '#type' => 'details',
+        '#title' => 'Article data configuration',
+        '#open' => TRUE,
+        '#tree' => TRUE,
+    ];
+      $title = $node->getTitle();
+      $body = $node->field_subheadline->value;
+      $config = $this->config(static::SETTINGS);
+      $form['article_data'][$nid . '_nid'] = [
+          '#type' => 'textfield',
+          '#title' => t('Queue Article ID'),
+          '#default_value' => $nid,
+      ];
+      $form['article_data'][$nid . '_title'] = [
+          '#type' => 'textfield',
+          '#title' => t('Queue Article title'),
+          '#default_value' => !empty($config->get($nid . '_title')) ? $config->get($nid . '_title') : $title,
+          '#description' => t("Title field"),
+      ];
+      $form['article_data'][$nid . '_body'] = [
+          '#type' => 'textarea',
+          '#title' => t('Queue Article summary'),
+          '#default_value' => !empty($config->get($nid . '_body')) ? $config->get($nid . '_body') : $body,
+          '#description' => t("Summary description"),
+      ];
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    // Retrieve the configuration.
+    $articles = $form_state->getValues();
+    foreach ($articles['article_data'] as $key => $value) {
+      $warning_value = ['article_data', $key];
+      $this->articleDataSave($form_state, $key , $warning_value);
+    }
+    parent::submitForm($form, $form_state);
+  }
+
+  /**
+   * Callback function to save the ConfigForm settings.
+   */
+  public function articleDataSave(&$form_state, $store_val, $value) {
+    $this->configFactory->getEditable(static::SETTINGS)
+        // Set the submitted configuration setting.
+        ->set($store_val, $form_state->getValue($value))
+        ->save();
+  }
+}
---
+++ sph_newsletter/src/Form/EditArticleConfigForm.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Drupal\sph_newsletter\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
+use Drupal\node\Entity\Node;
+
+
+/**
+ * Class EditArticleConfigForm
+ * @package Drupal\sph_newsletter\Form
+ */
+class EditArticleConfigForm extends FormBase {
+  const SETTINGS = 'example.settings';
+
+  public function getFormId()
+  {
+    return 'article_edit_form';
+    // TODO: Implement getFormId() method.
+  }
+
+  public function buildForm(array $form, FormStateInterface $form_state)
+  {
+    $nid = \Drupal::routeMatch()->getParameter('nid');
+    $node = Node::load($nid);
+    $queueArticles = $node->get('field_queue_articles')->getValue();
+    $form['article_data'] = [
+        '#type' => 'table',
+        '#header' => array(t('ID'), t('Title'), t('Summary'), t('Edit')),
+        '#title' => 'Article data configuration',
+        '#open' => TRUE,
+        '#tree' => TRUE,
+    ];
+    foreach ($queueArticles as $articles) {
+      $node = Node::load($articles['target_id']);
+      $title = $node->getTitle();
+      $body = $node->field_subheadline->value;
+      $config = $this->config(static::SETTINGS);
+      $form['article_data'][$articles['target_id']]['id'] = array(
+          '#plain_text' => $articles['target_id'],
+      );
+      $form['article_data'][$articles['target_id']]['title'] = array(
+          '#plain_text' => !empty($config->get($articles['target_id'] . '_title')) ? $config->get($articles['target_id'] . '_title') : $title,
+      );
+      $form['article_data'][$articles['target_id']]['body'] = array(
+          '#plain_text' => !empty($config->get($articles['target_id'] . '_body')) ? $config->get($articles['target_id'] . '_body') : $body,
+      );
+      $form['article_data'][$articles['target_id']]['operations'] = array(
+          '#type' => 'operations',
+          '#links' => array(),
+      );
+      $form['article_data'][$articles['target_id']]['operations']['#links']['edit'] = array(
+          'title' => t('Edit'),
+          'url' => Url::fromRoute('sph_newsletter.edit_article', array('id' => $articles['target_id'])),
+      );
+    }
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+
+  }
+}
